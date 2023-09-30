693c4ce32faf36f7ebdb97781fedbed55020f2ad
marcellopfc <marcello@pop-os.localdomain>
2023-09-30 09:59:32 -0300

    organizing by directories

---
+++ 1300/cards.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define dbg(x) cout<<#x <<" = "<< x << endl
+using namespace std;
+typedef long long ll;
+const int INF=0x3f3f3f3f;
+const ll LINF=0x3f3f3f3f3f3f3f3fll;
+ 
+void solve(){
+	string s;cin>>s;
+	
+	for(int i=0;i<s.size();i++){
+		if(s[i]=='n') cout<<"1 ";
+		else if(s[i]=='z') cout<<"0 ";
+	}cout<<endl;
+}
+ 
+ 
+int main(){_
+	bool multi=false;
+  if(multi){
+  	int c;cin>>c;
+    for(int i=0;i<c;i++){
+    	solve(); 
+    }
+		return 0;
+  }
+    solve();
+    return 0;
+}
